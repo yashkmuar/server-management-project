c3ce7b937ee58a7bd304f7bcd0fb120bee0eaffc
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-07 17:20:31 +0530

    traffic component added

---
+++ src/app/dashboard/traffic/traffic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-traffic',
+  standalone: true,
+  imports: [],
+  templateUrl: './traffic.component.html',
+  styleUrl: './traffic.component.css'
+})
+export class TrafficComponent {
+
+}
