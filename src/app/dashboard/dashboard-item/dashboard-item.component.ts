6ce2f8fc835da8627c35777040ab2998ed4a1a7a
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-07 18:54:45 +0530

    property bindin of image and title input signal

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -11,6 +11,6 @@ export class DashboardItemComponent {
   // @Input({required:true}) image!: {src:string, alt:string};
   // @Input({required:true}) title!:string;
 
-  image = input<{src:string, alt:string}>();
-  title = input<string>();
+  image = input.required<{src:string, alt:string}>();
+  title = input.required<string>();
 }
