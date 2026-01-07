487b81f4fa3761a5fd60ac5fc5eda895b50cac11
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-07 17:35:18 +0530

    adding of tickets selector into the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
