34dde5a713ede770aaaf49f1c38f2fac4c5d155d
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-07 17:50:13 +0530

    makes input properties of dashboard-item component as image and title

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-item.component.css'
 })
 export class DashboardItemComponent {
+  // @Input({required:true}) image!: {src:string, alt:string};
+  // @Input({required:true}) title!:string;
 
+  image = input<{src:string, alt:string}>();
+  title = input<string>();
 }
