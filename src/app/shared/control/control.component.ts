0775e15f62646db61758debf3de2a5245ad21e5f
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-08 18:36:35 +0530

    listnening of click events in host property

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +8,14 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)' : 'onClick()'
   }
 })
 export class ControlComponent {
   label = input.required<string>();
+
+  onClick() {
+    console.log('Clicked!');
+  }
 }
