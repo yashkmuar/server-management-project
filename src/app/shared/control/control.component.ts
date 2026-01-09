130183594cff6e4fa037d508bd517bb0d7b4ad93
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-09 09:57:23 +0530

    new-ticket element template variables projected using contentChild

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, ElementRef, HostBinding, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -15,8 +15,11 @@ import { Component, ElementRef, HostBinding, inject, input, ViewEncapsulation } 
 export class ControlComponent {
   label = input.required<string>();
   private el = inject(ElementRef);
+  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
 
   onClick() {
     console.log('Clicked!');
+    console.log(this.el);
+    console.log(this.control);
   }
 }
