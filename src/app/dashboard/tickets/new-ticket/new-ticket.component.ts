c8c2ec9090f9965c357dd3029d9f1de7cf89e8d4
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-09 10:03:43 +0530

    using of ngafterviewinit and ngonit method to see the flow of the application

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -10,9 +10,19 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit {
   //@ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit(): void {
+    console.log('ON INIT');
+    console.log(this.form().nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+    console.log(this.form().nativeElement);
+  }
 
   onSubmit(title: string, ticketext: string) {
     console.log(title);
