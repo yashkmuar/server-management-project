12e323c43a5532efeee601eecf097a54551c1553
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-09 21:34:07 +0530

    tickets are work in toggle mode, you can expand it to see what it is the ticket title and request it is for

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { Ticket } from '../ticket.model';
 
 @Component({
@@ -10,5 +10,9 @@ import { Ticket } from '../ticket.model';
 })
 export class TicketComponent {
   data = input.required<Ticket>();
+  detailsVisible = signal(false);
 
+  onToggleDetails(){
+    this.detailsVisible.set(!this.detailsVisible());
+  }
 }
